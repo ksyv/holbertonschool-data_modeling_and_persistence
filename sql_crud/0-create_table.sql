1ec10e4eb6944e0aa84873c34fce14b3ba67f72d
ksyv <ksyviais@gmail.com>
2026-04-21 16:25:02 +0200

    Create folder, readme and first file for create table

---
+++ sql_crud/0-create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0,
+    published_year INTEGER NOT NULL
+)
